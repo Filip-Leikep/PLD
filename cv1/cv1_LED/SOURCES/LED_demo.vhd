d63ee5a13786d10fae661a205d916617f82254f9
Filip-Leikep <xleike01@vutbr.cz>
2025-02-14 14:39:44 +0100

    hotovo

--- cv1/cv1_LED/SOURCES/LED_demo.vhd
+++ cv1/cv1_LED/SOURCES/LED_demo.vhd
@@ -1,45 +1,31 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/14/2025 12:56:02 PM
--- Design Name: 
--- Module Name: LED_demo - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+----------------------------------------------------------------------------------
 entity LED_demo is
     Port ( BTN : in STD_LOGIC_VECTOR (1 to 4);
            SW : in STD_LOGIC_VECTOR (1 to 4);
            LED : out STD_LOGIC_VECTOR (7 downto 0));
 end LED_demo;
-
+-----------------------------------------------------------------------------------
 architecture Behavioral of LED_demo is
 
 begin
 
+---prvni ukol LED START
+    --LED(0) <=     BTN(1);
+    --LED(1) <= NOT BTN(2);
+    
+    --- prvni moznost LED(2) <= '1'; pro kazdou LED    
+    --LED(7 DOWNTO 4) <= "0000";
+---prvni ukol END    
 
+---druhy ukol KODOVY ZAMEK START
+---heslo BTN1-4: 0110 SW1-4: 1101
+    
+    LED(7 DOWNTO 1) <= "0000000";
+    LED(0) <= '1' WHEN BTN = "0110" AND SW = "1101" ELSE
+              '0';
+              
+---druhy ukol END
+    
 end Behavioral;
